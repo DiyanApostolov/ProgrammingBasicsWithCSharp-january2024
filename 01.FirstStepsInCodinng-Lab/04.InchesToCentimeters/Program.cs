30bef9970c2151022a0c26cc5ba69404104121b1
Diyan Apostolov <diyan.apostolov@yahoo.com>
2024-01-08 09:43:20 +0200

    Add solutions for first Lab

---
+++ 01.FirstStepsInCodinng-Lab/04.InchesToCentimeters/Program.cs
@@ -0,0 +1,5 @@
+﻿double inches = double.Parse(Console.ReadLine());
+
+double centimetres = inches * 2.54;
+
+Console.WriteLine(centimetres);
---
+++ 01.FirstStepsInCodinng-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,9 @@
+﻿double yardArea = double.Parse(Console.ReadLine());
+
+double price = yardArea * 7.61;
+
+double discount = price * 0.18;
+double finalPrice = price - discount;
+
+Console.WriteLine($"The final price is: {finalPrice} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
